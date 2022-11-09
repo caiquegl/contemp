f56085c31486533703585245bdf7a849f38b9ab2
ranoob <whyfck@gmail.com>
2022-11-09 13:51:23 -0300

    css adjustments on Container

--- src/container/Container.tsx
+++ src/container/Container.tsx
@@ -1,10 +1,11 @@
 import { Box, Container as ContainerChakra } from "@chakra-ui/react";
 import React from "react";
+import { pxToRem } from "../utils/pxToRem";
 
 export const Container = ({ children }: any) => {
   return (
     <Box w="100%" bg="black.800">
-      <ContainerChakra maxW="6xl" p="12px 0 31px">
+      <ContainerChakra maxW="6xl" p={`${pxToRem(12)} 0 ${pxToRem(31)}`}>
         {children}
       </ContainerChakra>
     </Box>
