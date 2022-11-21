3d67741e280f761a6cb2423046debcb0bbc38e9f
ranoob <whyfck@gmail.com>
2022-11-21 15:56:56 -0300

    change menu style due to design changes

---
+++ src/components/HeaderMenu.tsx
@@ -0,0 +1,39 @@
+import { TriangleDownIcon } from "@chakra-ui/icons"
+import { Menu, MenuItem, MenuButton, MenuList, Link } from "@chakra-ui/react"
+
+export type MenuItemProps = {
+  label: string
+  link: string
+}
+
+export type MenuProps = {
+  menuItems?: MenuItemProps[]
+  title: string
+}
+
+const CustomMenuItem = ({ label, link }: MenuItemProps) => (
+  <MenuItem as={Link} href={link} _hover={{ background: 'red.600' }}>
+    {label}
+  </MenuItem>
+)
+
+export const HeaderMenu = ({ title, menuItems }: MenuProps) => {
+  return (
+    <Menu isLazy closeOnBlur matchWidth>
+      <MenuButton
+        py={2}
+        transition='all 0.2s'
+        w="max-content"
+        display="flex"
+      >
+        {title} <TriangleDownIcon />
+      </MenuButton>
+
+      <MenuList background="black.800">
+        {menuItems?.map(({ label, link }) => (
+          <CustomMenuItem label={label} link={link} />
+        ))}
+      </MenuList>
+    </Menu>
+  )
+}
