ef88201be990d124b32c8bd6566a0fffc74ad130
ranoob <whyfck@gmail.com>
2022-11-21 08:01:05 -0300

    img component to avoid duplication

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,19 +1,24 @@
 import { Box, Image as ChakraImage, BoxProps } from "@chakra-ui/react";
+import { StaticImageData } from "next/image";
+import { pxToRem } from "../utils/pxToRem";
 
 type ImageProps = {
-  src: string
+  src: string | StaticImageData
   alt?: string
 } & BoxProps
 
 export const Image = ({ src, alt, ...props }: ImageProps) => {
+  const bgImage = typeof src === 'string' ? `url('${src}')` : src.src
+
   return (
     <Box
-      bgImage={`url('${src}')`}
+      bgImage={bgImage}
       bgSize="85%"
       bgRepeat="no-repeat"
       bgPosition="center"
       w="100%"
       h="100%"
+      minH={pxToRem(200)}
       flex={1}
       {...props}
     >
