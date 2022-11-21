264377d6085af018536a1d315c4d11a4e7f520de
ranoob <whyfck@gmail.com>
2022-11-21 07:59:22 -0300

    responsive and adjustments

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -2,6 +2,7 @@ import { Box, Button, Flex, HStack, Icon, Text } from "@chakra-ui/react";
 import React from "react";
 import { BiPhone } from "react-icons/bi";
 import { AiOutlineMail } from "react-icons/ai";
+import { pxToRem } from "../utils/pxToRem";
 
 export const Catalog = () => {
   return (
@@ -34,31 +35,51 @@ export const Catalog = () => {
           entrar em contato e solicitar para um de nossos representantes.
           Através do número (11) 4223-5140 ou pelo e-mail vendas@contemp.com.br
         </Text>
-        <Flex alignItems="center" justifyContent="center">
-          <HStack spacing="24px">
-            <Button
-              w="179px"
-              h="50px"
-              borderRadius="25px"
-              bg="red.600"
-              fontSize="20px"
-              _hover={{ transition: "all 0.5s", opacity: 0.7 }}
-            >
-              <Icon as={BiPhone} mr="10px" />
-              Telefonar
-            </Button>
-            <Button
-              w="179px"
-              h="50px"
-              borderRadius="25px"
-              bg="red.600"
-              fontSize="20px"
-              _hover={{ transition: "all 0.5s", opacity: 0.7 }}
-            >
-              <Icon as={AiOutlineMail} mr="10px" />
-              Enviar e-mail
-            </Button>
-          </HStack>
+
+        <Flex
+          alignItems="center"
+          justifyContent="space-between"
+          flexWrap="wrap"
+          w="100%"
+          maxW={pxToRem(380)}
+          margin="auto"
+          h={{
+            base: 120
+          }}
+          flexDirection={{
+            base: 'column',
+            md: 'row'
+          }}
+        >
+          <Button
+            width={{
+              base: pxToRem(279),
+              md: pxToRem(179),
+            }}
+            h="50px"
+            borderRadius="25px"
+            bg="red.600"
+            fontSize={pxToRem(20)}
+            _hover={{ transition: "all 0.5s", opacity: 0.7 }}
+          >
+            <Icon as={BiPhone} mr="10px" />
+            Telefonar
+          </Button>
+
+          <Button
+            width={{
+              base: pxToRem(279),
+              md: pxToRem(179),
+            }}
+            h="50px"
+            borderRadius="25px"
+            bg="red.600"
+            fontSize={pxToRem(20)}
+            _hover={{ transition: "all 0.5s", opacity: 0.7 }}
+          >
+            <Icon as={AiOutlineMail} mr="10px" />
+            Enviar e-mail
+          </Button>
         </Flex>
       </Box>
     </Flex>
