700e5baac76e617335614bbb31b7bde2a159446a
Raphael Garcia <raphael.henriquegarcia@gmail.com>
2022-12-10 12:57:48 -0300

    add scripts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -33,6 +33,28 @@ export default class MyDocument extends Document {
           <Main />
           <NextScript />
           <script type="text/javascript" async src="https://d335luupugsy2.cloudfront.net/js/loader-scripts/e038acad-48ed-4f16-8e46-7c675b617c92-loader.js" ></script>
+          
+          <script dangerouslySetInnerHTML={{
+            __html: `
+              !function (t, e, c, n) {
+                var s = e.createElement(c);
+                s.async = 1, s.src = 'https://scripts.claspo.io/scripts/' + n + '.js';
+                var r = e.scripts[0];
+                r.parentNode.insertBefore(s, r);
+                var f = function () {
+                    f.c(arguments);
+                };
+                f.q = [];
+                f.c = function () {
+                    f.q.push(arguments);
+                };
+                t['claspo'] = t['claspo'] || f;
+              }(window, document, 'script', 'D8ABC933B8D7450EB4285DE2F58D1A8D');
+
+              
+              claspo('init');
+            `,
+          }} />
         </body>
       </Html>
     );
