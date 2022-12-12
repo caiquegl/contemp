78179ca98348666b24289db44767fc740c49c262
ranoob <whyfck@gmail.com>
2022-12-12 14:27:46 -0300

    feat: added google optmize script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,11 +4,13 @@ import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "../styles/theme";
 import { AuthProvider } from "../contextAuth/authContext";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <AuthProvider>
+        <Script src="https://www.googleoptimize.com/optimize.js?id=OPT-W2XSF2X" />
         <Component {...pageProps} />
       </AuthProvider>
     </ChakraProvider>
