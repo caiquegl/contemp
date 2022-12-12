44d365d8d9df8957239b2f9e8de5cb269985f28a
ranoob <whyfck@gmail.com>
2022-12-12 20:06:37 -0300

    fix: z-index

--- src/utils/customSwiperBullets.ts
+++ src/utils/customSwiperBullets.ts
@@ -1,9 +1,9 @@
 import { pxToRem } from "./pxToRem";
 
 export function customSwiperBullets(_index: number, className: string) {
-  return `<span style="width: ${pxToRem(13)}; height: ${pxToRem(
-    13
-  )}; bottom: ${pxToRem(15)}; border-radius: ${pxToRem(
-    100
-  )};" class=${className}></span>`;
+  const width_height = pxToRem(13);
+
+  return `<span z-index: 999; style="width: ${width_height}; height: ${width_height}; bottom: ${pxToRem(
+    15
+  )}; border-radius: ${pxToRem(100)};" class=${className}></span>`;
 }
