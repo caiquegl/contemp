06578dd86fffc7fb011ef5c8bb7997ba3cea90c0
ranoob <whyfck@gmail.com>
2022-12-12 20:06:23 -0300

    separated colors const

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,33 +1,35 @@
-import { extendTheme } from '@chakra-ui/react'
+import { extendTheme } from "@chakra-ui/react";
+
+export const colors = {
+  black: {
+    "900": "#232323",
+    "800": "#242424", // text strong
+    "200": "#393939",
+    "50": "#D6D6D6",
+    "100": "#E2E2E2",
+  },
+  red: {
+    "600": "#B60005",
+  },
+  white: {
+    "500": "#F7F7F7",
+    "700": "#EFEFEF",
+    "900": "#D1D1D1",
+  },
+};
 
 export const theme = extendTheme({
-  colors: {
-    black: {
-      "900": "#232323",
-      "800": "#242424", // text strong
-      "200": "#393939",
-      "50": "#D6D6D6",
-      "100": "#E2E2E2"
-    },
-    red: {
-        "600": "#B60005"
-    },
-    white: {
-      "500": "#F7F7F7",
-      "700": "#EFEFEF",
-      "900": "#D1D1D1"
-    }
-  },
+  colors,
   fonts: {
-    heading: 'PT Sans',
-    body: 'PT Sans'
+    heading: "PT Sans",
+    body: "PT Sans",
   },
   styles: {
     global: {
       body: {
-        bg: 'black.900',
-        color: 'white'
-      }
-    }
-  }
-})
+        bg: "black.900",
+        color: "white",
+      },
+    },
+  },
+});
