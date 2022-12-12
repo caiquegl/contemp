39b8c4508bf51df5c46b6ff28dceefbe405bff33
ranoob <whyfck@gmail.com>
2022-12-12 19:53:41 -0300

    remove unused imports

--- src/components/CKEditor.js
+++ src/components/CKEditor.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 export default function CKeditor({ onChange, editorLoaded, name, value }) {
   const editorRef = useRef();
   const { CKEditor, ClassicEditor } = editorRef.current || {};
