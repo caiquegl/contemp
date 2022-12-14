09ac758b915bf39602e7d375f714b92896dcee93
ranoob <whyfck@gmail.com>
2022-12-14 12:05:31 -0300

    fix: placeholder

--- src/components/ContainerHome/index.tsx
+++ src/components/ContainerHome/index.tsx
@@ -5,11 +5,11 @@ import {
   Checkbox,
   Button,
   Flex,
-  useToast,
-} from "@chakra-ui/react";
-import { useEffect, useRef, useState } from "react";
-import { database, initFirebase } from "../../utils/db";
-import InputsHome from "./inputs";
+  useToast
+} from '@chakra-ui/react'
+import { useEffect, useRef, useState } from 'react'
+import { database, initFirebase } from '../../utils/db'
+import InputsHome from './inputs'
 import {
   addDoc,
   collection,
@@ -19,60 +19,60 @@ import {
   query,
   where,
   updateDoc,
-  doc,
-} from "firebase/firestore";
-import { InputDefault } from "../Form/Input";
-import { SelectDefault } from "../Form/Select";
-import { TextareaDefault } from "../Form/Textarea";
-import { useForm } from "react-hook-form";
-import { ViewImage } from "../ContainerAddProduct/ViewImage";
+  doc
+} from 'firebase/firestore'
+import { InputDefault } from '../Form/Input'
+import { SelectDefault } from '../Form/Select'
+import { TextareaDefault } from '../Form/Textarea'
+import { useForm } from 'react-hook-form'
+import { ViewImage } from '../ContainerAddProduct/ViewImage'
 
 const ContainerHome = ({ indexProduct, defaultValues, reset }: any) => {
-  initFirebase();
-  const toast = useToast();
-  const formRef = useRef<any>();
-  const [loading, setLoading] = useState(false);
-  const [hasCarrocel, setHasCarrocel] = useState(false);
-  const [list, setList] = useState<any>([]);
-  const [urls, setUrls] = useState<any>([]);
-  const [icon, setIcon] = useState<any>("");
-  const { register, handleSubmit, formState, setValue } = useForm({});
+  initFirebase()
+  const toast = useToast()
+  const formRef = useRef<any>()
+  const [loading, setLoading] = useState(false)
+  const [hasCarrocel, setHasCarrocel] = useState(false)
+  const [list, setList] = useState<any>([])
+  const [urls, setUrls] = useState<any>([])
+  const [icon, setIcon] = useState<any>('')
+  const { register, handleSubmit, formState, setValue } = useForm({})
 
-  const { errors } = formState;
+  const { errors } = formState
 
   const saveProduct = async (body: any) => {
     try {
       if (urls.length === 0 || !!!icon) {
         toast({
-          title: "Erro",
-          description: "Foto da imagem e icone são obrigatórios",
-          status: "error",
+          title: 'Erro',
+          description: 'Foto da imagem e icone são obrigatórios',
+          status: 'error',
           duration: 3000,
-          isClosable: true,
-        });
-        return;
+          isClosable: true
+        })
+        return
       }
-      setLoading(true);
+      setLoading(true)
 
-      const dbInstance = collection(database, "home");
-      let exist = false;
-      let bodyExist: any = {};
+      const dbInstance = collection(database, 'home')
+      let exist = false
+      let bodyExist: any = {}
 
       const qExist = query(
         dbInstance,
-        where("indexProduct", "==", indexProduct),
+        where('indexProduct', '==', indexProduct),
         limit(1)
-      );
+      )
 
       await getDocs(qExist).then((data) => {
         if (data.docs.length > 0) {
-          exist = true;
+          exist = true
           bodyExist = {
             id: data.docs[0].id,
-            ref: data.docs[0].ref,
-          };
+            ref: data.docs[0].ref
+          }
         }
-      });
+      })
 
       if (!exist) {
         await addDoc(dbInstance, {
@@ -80,84 +80,84 @@ const ContainerHome = ({ indexProduct, defaultValues, reset }: any) => {
           indexProduct,
           destaque: hasCarrocel,
           urls,
-          icon,
-        });
+          icon
+        })
 
         toast({
-          title: "Sucesso",
-          description: "Produto cadastradado com sucesso.",
-          status: "success",
+          title: 'Sucesso',
+          description: 'Produto cadastradado com sucesso.',
+          status: 'success',
           duration: 3000,
-          isClosable: true,
-        });
+          isClosable: true
+        })
       } else {
-        const dbInstanceUpdate = doc(database, "home", bodyExist.id);
+        const dbInstanceUpdate = doc(database, 'home', bodyExist.id)
 
         await updateDoc(dbInstanceUpdate, {
           ...body,
           indexProduct,
           destaque: hasCarrocel,
           urls,
-          icon,
-        });
+          icon
+        })
         toast({
-          title: "Sucesso",
-          description: "Produto atualizado com sucesso.",
-          status: "success",
+          title: 'Sucesso',
+          description: 'Produto atualizado com sucesso.',
+          status: 'success',
           duration: 3000,
-          isClosable: true,
-        });
+          isClosable: true
+        })
       }
     } catch (error) {
       toast({
-        title: "Erro",
-        description: "Erro ao salvar produto",
-        status: "error",
+        title: 'Erro',
+        description: 'Erro ao salvar produto',
+        status: 'error',
         duration: 3000,
-        isClosable: true,
-      });
+        isClosable: true
+      })
     } finally {
-      setLoading(false);
-      reset();
+      setLoading(false)
+      reset()
     }
-  };
+  }
 
   const listCategory = async () => {
     try {
-      const dbInstance = collection(database, "categories");
-      let newList: any = [];
-      const q = query(dbInstance, orderBy("order", "asc"));
+      const dbInstance = collection(database, 'categories')
+      let newList: any = []
+      const q = query(dbInstance, orderBy('order', 'asc'))
       await getDocs(q).then((data) => {
         data.docs.forEach((doc) => {
-          newList.push({ ...doc.data(), id: doc.id, ref: doc.ref });
-        });
-      });
+          newList.push({ ...doc.data(), id: doc.id, ref: doc.ref })
+        })
+      })
 
-      setList(newList);
+      setList(newList)
     } catch (error) {
       toast({
-        title: "Erro",
-        description: "Erro ao listar categoria",
-        status: "error",
+        title: 'Erro',
+        description: 'Erro ao listar categoria',
+        status: 'error',
         duration: 3000,
-        isClosable: true,
-      });
+        isClosable: true
+      })
     }
-  };
+  }
 
   useEffect(() => {
-    listCategory();
-  }, []);
+    listCategory()
+  }, [])
 
   useEffect(() => {
-    setValue("name", defaultValues?.name);
-    setValue("category", defaultValues?.category);
-    setValue("description", defaultValues?.description);
-    setValue("link_name", defaultValues?.link_name);
-    setHasCarrocel(defaultValues && defaultValues.hasCarrocel ? true : false);
-    setUrls(defaultValues && defaultValues.urls ? defaultValues.urls : []);
-    setIcon(defaultValues && defaultValues.icon ? defaultValues.icon : "");
-  }, [defaultValues]);
+    setValue('name', defaultValues?.name)
+    setValue('category', defaultValues?.category)
+    setValue('description', defaultValues?.description)
+    setValue('link_name', defaultValues?.link_name)
+    setHasCarrocel(defaultValues && defaultValues.hasCarrocel ? true : false)
+    setUrls(defaultValues && defaultValues.urls ? defaultValues.urls : [])
+    setIcon(defaultValues && defaultValues.icon ? defaultValues.icon : '')
+  }, [defaultValues])
 
   return (
     <Box
@@ -175,9 +175,9 @@ const ContainerHome = ({ indexProduct, defaultValues, reset }: any) => {
           <InputDefault
             label="Nome do produto"
             type="text"
-            placeholder="nome do produto"
+            placeholder="Nome do produto"
             error={errors.name}
-            {...register("name", { required: "Nome é obrigatório" })}
+            {...register('name', { required: 'Nome é obrigatório' })}
           />
           <SelectDefault
             label="Categoria"
@@ -186,10 +186,10 @@ const ContainerHome = ({ indexProduct, defaultValues, reset }: any) => {
             opt={list.map((value: any) => {
               return {
                 name: value.name,
-                value: value.id,
-              };
+                value: value.id
+              }
             })}
-            {...register("category", { required: "Campo obrigatório" })}
+            {...register('category', { required: 'Campo obrigatório' })}
           />
         </HStack>
         <HStack w="100%" spacing="20px">
@@ -198,7 +198,7 @@ const ContainerHome = ({ indexProduct, defaultValues, reset }: any) => {
             type="text"
             placeholder="Link do Produto & Botão"
             error={errors.link_name}
-            {...register("link_name", { required: "Link é obrigatório" })}
+            {...register('link_name', { required: 'Link é obrigatório' })}
           />
         </HStack>
         <HStack w="100%" spacing="20px">
@@ -215,11 +215,11 @@ const ContainerHome = ({ indexProduct, defaultValues, reset }: any) => {
                   <ViewImage
                     url={value}
                     remove={() => {
-                      let newList: any = [];
+                      let newList: any = []
                       urls.forEach((value: any, indexRemove: number) => {
-                        if (index != indexRemove) newList.push(value);
-                      });
-                      setUrls(newList);
+                        if (index != indexRemove) newList.push(value)
+                      })
+                      setUrls(newList)
                     }}
                   />
                 ))}
@@ -232,15 +232,15 @@ const ContainerHome = ({ indexProduct, defaultValues, reset }: any) => {
               getUrls={(link: any) => setIcon(link)}
             />
             <Box marginTop={5} w="200px">
-              {icon && <ViewImage url={icon} remove={() => setIcon("")} />}
+              {icon && <ViewImage url={icon} remove={() => setIcon('')} />}
             </Box>
           </Box>
         </HStack>
         <TextareaDefault
           label="Descrição"
           error={errors.description}
-          {...register("description", {
-            required: "Descrição é obrigatório",
+          {...register('description', {
+            required: 'Descrição é obrigatório'
           })}
         />
         <Box w="100%">
@@ -270,13 +270,13 @@ const ContainerHome = ({ indexProduct, defaultValues, reset }: any) => {
           h="47px"
           isLoading={loading}
           type="submit"
-          _hover={{ transition: "all 0.4s", opacity: 0.7 }}
+          _hover={{ transition: 'all 0.4s', opacity: 0.7 }}
         >
           Salvar
         </Button>
       </Flex>
     </Box>
-  );
-};
+  )
+}
 
-export default ContainerHome;
+export default ContainerHome
