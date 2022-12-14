8d7062da22f33d9b155bf591aab6c6475882ef59
kemelinpina <78272062+kemelinpina@users.noreply.github.com>
2022-12-14 15:35:46 -0300

    kemi

--- next.config.js
+++ next.config.js
@@ -6,6 +6,26 @@ module.exports = {
         destination: '/trabalhe-conosco',
         permanent: true,
       },
+      {
+        source: '/contato',
+        destination: 'https://contemp.com.br',
+        permanent: true,
+      },
+      {
+        source: '/empresa',
+        destination: '/a-contemp',
+        permanent: true,
+      },
+      {
+        source: '/laboratorio-de-calibracao',
+        destination: '/calibracao',
+        permanent: true,
+      },
+      {
+        source: '/blog',
+        destination: 'https://blog.contemp.com.br/',
+        permanent: true,
+      },
     ]
   },
 }
