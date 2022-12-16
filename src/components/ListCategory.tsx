42916e390be11419a1d04843f926d043d0369ddb
ranoob <whyfck@gmail.com>
2022-12-16 18:25:58 -0300

    feat: added forwardRef

--- src/components/ListCategory.tsx
+++ src/components/ListCategory.tsx
@@ -1,17 +1,17 @@
-import { Flex, Grid } from "@chakra-ui/react";
-import React from "react";
-import { pxToRem } from "../utils/pxToRem";
-import CardProductWithDescription from "./CardProductWithDescription";
+import { Flex, Grid } from '@chakra-ui/react'
+import React, { forwardRef } from 'react'
+import { pxToRem } from '../utils/pxToRem'
+import CardProductWithDescription from './CardProductWithDescription'
 
-export const ListCategory = ({ bg, data }: any) => {
+export const ListCategory = forwardRef(({ bg, data }: any, ref: any) => {
   return (
-    <Flex w="100%" bg={bg}>
+    <Flex w="100%" bg={bg} ref={ref}>
       <Grid
         margin={`${pxToRem(80)} 0`}
         templateColumns={`repeat(auto-fill, minmax(${pxToRem(320)}, 1fr))`}
         columnGap={pxToRem(30)}
         p={{
-          xl: "0 20px",
+          xl: '0 20px'
         }}
         w="100%"
       >
@@ -19,19 +19,19 @@ export const ListCategory = ({ bg, data }: any) => {
           data.length > 0 &&
           data.map((catg: any) => (
             <CardProductWithDescription
-              img={catg.urls && catg.urls[0] ? catg.urls[0] : ""}
+              img={catg.urls && catg.urls[0] ? catg.urls[0] : ''}
               text={catg?.name}
               description={catg?.description}
               color={
-                bg === "red.600" || bg === "black.800" ? "white" : undefined
+                bg === 'red.600' || bg === 'black.800' ? 'white' : undefined
               }
-              buttomBottom={bg === "red.600" ? "white" : undefined}
+              buttomBottom={bg === 'red.600' ? 'white' : undefined}
               containerProps={{
-                margin: "auto",
+                margin: 'auto'
               }}
             />
           ))}
       </Grid>
     </Flex>
-  );
-};
+  )
+})
