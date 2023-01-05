3ea457fa9dadb287a9dc82dcc1d58c651a41f424
caiquegl <arq.caique@hotmail.com>
2023-01-05 18:27:12 -0300

    email

--- src/pages/api/mail.ts
+++ src/pages/api/mail.ts
@@ -11,10 +11,10 @@ export default async (req: any, response: any) => {
         SibApiV3Sdk.ApiClient.instance.authentications['api-key'].apiKey = process.env.SENDBLUE
         await new SibApiV3Sdk.TransactionalEmailsApi().sendTransacEmail({
           "sender":{ "email":"marketing@contemp.com.br", "name":"Contemp"},
-          "subject": body.id ? 'Novo contato' : 'Orçamento',
+          "subject": body.id ? body.id == 'vagas' ? 'Novo currículo' : 'Novo contato' : 'Orçamento',
           "htmlContent":body.id ? HtmlDefault(body.name, body.email, body.empresa,body.telephone,  body.description, body.file) : HtmlOrcamento(body.name, body.email, '',body.telephone,  body.description, '', body.products ),
           "messageVersions":[
-            { "to": [{ "email": 'vendas@contemp.com.br', name: 'contemp' }],
+            { "to": [{ "email": body.id && body.id == 'vagas' ? 'rh@contemp.com.br' : 'vendas@contemp.com.br', name: 'contemp' }],
             "cc": [{ "email": 'arq.caique@hotmail.com', name: 'caique' }, { "email": 'kemelin@3hub.co', name: 'Kemilin' }],
            }
           ]
