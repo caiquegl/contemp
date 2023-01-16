5cbdbef0ee4212abbd9af7e9bc28334d760f6e81
caiquegl <arq.caique@hotmail.com>
2023-01-16 15:32:39 -0300

    merge

--- src/pages/site-map.tsx
+++ src/pages/site-map.tsx
@@ -5,7 +5,6 @@ import { useAuth } from '../contextAuth/authContext'
 import { useRouter } from 'next/router'
 import { v4 as uuidv4 } from 'uuid'
 import { Header } from '../components/Header'
-// import  {updateSiteMapXML}  from './api/sitemap.xml'
 
 const SiteMapContainer = () => {
   const router = useRouter()
@@ -23,17 +22,16 @@ const SiteMapContainer = () => {
         }),
       }
     })
-    // updateSiteMapXML(allCategory,allProductsActive);
     setListCategory(
-    newListCategory.sort(function (a:any, b:any) {
-      return a.products.length - b.products.length
-    }))
-
+      newListCategory.sort(function (a: any, b: any) {
+        return a.products.length - b.products.length
+      })
+    )
   }, [allCategory, allProductsActive])
 
   return (
     <>
-          {/* <Head>
+      {/* <Head>
         <meta
           name="description"
           content="Procurando medição e controle de temperatura em processos industrais? A Contemp é pioneiro no Brasil. Confira!"
@@ -46,63 +44,53 @@ const SiteMapContainer = () => {
         <link rel="icon" href="/favicon.png" />
       </Head> */}
       <Header />
-      <Flex
-        w="100%"
-        alignItems="center"
-        justifyContent="center"
-        direction="column"
-        h="250px"
-      >
-        <Text fontSize="45px" fontWeight="bold" textAlign="center">
+      <Flex w='100%' alignItems='center' justifyContent='center' direction='column' h='250px'>
+        <Text fontSize='45px' fontWeight='bold' textAlign='center'>
           Site Map
         </Text>
       </Flex>
-    <Container
-      maxW='7xl'
-      p={['40px 20px 31px', '40px 20px 31px', '40px 20px 31px', '40px 20px 31px', '40px 10px 31px']}
-    >
-      <Grid
-        templateColumns={['repeat(1, 1fr)', 'repeat(1, 1fr)', 'repeat(2, 1fr)', 'repeat(2, 1fr)', 'repeat(3, 1fr)']}
-        w='100%'
+      <Container
+        maxW='7xl'
+        p={['40px 20px 31px', '40px 20px 31px', '40px 20px 31px', '40px 20px 31px', '40px 10px 31px']}
       >
-        {listCategory &&
-          listCategory.length > 0 &&
-          listCategory.map((el: any, index: number) => (
-            <GridItem w='100%' gap={6} key={uuidv4()} px='20px'>
-              <Text
-                fontWeight='bold'
-                fontSize='20px'
-                mb='20px'
-                cursor='pointer'
-                onClick={() => router.push(`/category/${el.name.replaceAll(' ', '_')}#viewCategory`)}
-              >
-                {el.name}
-              </Text>
-              {el.products &&
-                el.products.length > 0 &&
-                el.products.map((el: any, index: number) => (
-                  <GridItem w='100%' gap={6} key={uuidv4()}>
-                    <Text
-                      fontSize='15px'
-                      mb='20px'
-                      cursor='pointer'
-                      onClick={() => router.push(`/produto/${el.name.replaceAll(' ', '_')}`)}
-                    >
-                      {el.name}
-                    </Text>
-                  </GridItem>
-                ))}
-                {
-                listCategory.length - 1 != index && <Divider mb='20px' />
-                }
-            </GridItem>
-          ))}
-      </Grid>
-    </Container>
+        <Grid
+          templateColumns={['repeat(1, 1fr)', 'repeat(1, 1fr)', 'repeat(2, 1fr)', 'repeat(2, 1fr)', 'repeat(3, 1fr)']}
+          w='100%'
+        >
+          {listCategory &&
+            listCategory.length > 0 &&
+            listCategory.map((el: any, index: number) => (
+              <GridItem w='100%' gap={6} key={uuidv4()} px='20px'>
+                <Text
+                  fontWeight='bold'
+                  fontSize='20px'
+                  mb='20px'
+                  cursor='pointer'
+                  onClick={() => router.push(`/category/${el.name.replaceAll(' ', '_')}#viewCategory`)}
+                >
+                  {el.name}
+                </Text>
+                {el.products &&
+                  el.products.length > 0 &&
+                  el.products.map((el: any, index: number) => (
+                    <GridItem w='100%' gap={6} key={uuidv4()}>
+                      <Text
+                        fontSize='15px'
+                        mb='20px'
+                        cursor='pointer'
+                        onClick={() => router.push(`/produto/${el.name.replaceAll(' ', '_')}`)}
+                      >
+                        {el.name}
+                      </Text>
+                    </GridItem>
+                  ))}
+                {listCategory.length - 1 != index && <Divider mb='20px' />}
+              </GridItem>
+            ))}
+        </Grid>
+      </Container>
     </>
   )
 }
 
 export default SiteMapContainer
-
-
