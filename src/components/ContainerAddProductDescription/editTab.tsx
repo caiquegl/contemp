f4915981e5e70d685acadda86eaa5dac168c2473
caiquegl <arq.caique@hotmail.com>
2023-01-27 12:52:34 -0300

    see

--- src/components/ContainerAddProductDescription/editTab.tsx
+++ src/components/ContainerAddProductDescription/editTab.tsx
@@ -1,61 +1,61 @@
-import {
-    Box,
-    Text,
-    Flex,
-    Icon,
-    Textarea,
-} from "@chakra-ui/react";
+import { Box, Text, Flex, Icon, Textarea } from '@chakra-ui/react'
 
 import { AiFillEye } from 'react-icons/ai'
-import { useState } from "react";
+import { useState } from 'react'
 import { Editor } from '../EditorFile'
-import dynamic from "next/dynamic";
+import dynamic from 'next/dynamic'
 const CustomEditor = dynamic(() => import('../CustomEditor'), { ssr: false })
 
 const EditTab = ({ tabs, index, setTabs, editorLoaded, load }: any) => {
-    const [see, setSee] = useState(true)
-    let [value, setValue] = useState(tabs[index]?.text ? tabs[index]?.text : "")
+  const [see, setSee] = useState(false)
+  let [value, setValue] = useState(tabs[index]?.text ? tabs[index]?.text : '')
 
-    let handleInputChange = (e: any) => {
-        let inputValue = e.target.value
-        setValue(inputValue)
-        let newList = tabs;
-        newList[index].text = inputValue;
-        setTabs(newList);
-    }
+  let handleInputChange = (e: any) => {
+    let inputValue = e.target.value
+    setValue(inputValue)
+    let newList = tabs
+    newList[index].text = inputValue
+    setTabs(newList)
+  }
 
-    return (
-        <Box>
-            <Flex alignItems="center">
-                <Text color="black.800" fontSize="20px" mb="10px">
-                    Conteúdo da tab
-                </Text>
-                <Icon ml="10px" as={AiFillEye} color="black.800" fontSize="20px" cursor="pointer" onClick={() => {
-                    load()
-                    setSee(!see)
-                }} />
-            </Flex>
-            <Box color="black.800">
-                {see ?
-                    <Textarea value={value} onChange={handleInputChange} h="200px" />
-                    :
-                    <div>
-                        <CustomEditor
-                            name="description"
-                            onChange={(evt: any) => {
-                                let newList = tabs;
-                                newList[index].text = evt;
-                                setTabs(newList);
-                            }}
-                            value={tabs[index]?.text ? tabs[index]?.text : ""}
-                            editorLoaded={editorLoaded}
-                        />
-                    </div>
+  return (
+    <Box>
+      <Flex alignItems='center'>
+        <Text color='black.800' fontSize='20px' mb='10px'>
+          Conteúdo da tab
+        </Text>
+        <Icon
+          ml='10px'
+          as={AiFillEye}
+          color='black.800'
+          fontSize='20px'
+          cursor='pointer'
+          onClick={() => {
+            load()
+            setSee(!see)
+          }}
+        />
+      </Flex>
+      <Box color='black.800'>
+        {see ? (
+          <Textarea value={value} onChange={handleInputChange} h='200px' />
+        ) : (
+          <div>
+            <CustomEditor
+              name='description'
+              onChange={(evt: any) => {
+                let newList = tabs
+                newList[index].text = evt
+                setTabs(newList)
+              }}
+              value={tabs[index]?.text ? tabs[index]?.text : ''}
+              editorLoaded={editorLoaded}
+            />
+          </div>
+        )}
+      </Box>
+    </Box>
+  )
+}
 
-                }
-            </Box>
-        </Box>
-    );
-};
-
-export default EditTab;
+export default EditTab
