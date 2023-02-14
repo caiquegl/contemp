a00b9cf9058b96bb6e7466ac1c9b68d0bafe525c
caiquegl <arq.caique@hotmail.com>
2023-02-14 16:07:49 -0300

    decode url

--- src/utils/replaceNameToUrl.ts
+++ src/utils/replaceNameToUrl.ts
@@ -1,7 +1,7 @@
 export const replaceNameToUrl = (text: string) => {
-    return encodeURI(text).replaceAll('/', '||')
+    return encodeURI(text.replaceAll(' ', '_').replaceAll('/', '||'))
 }
 
 export const decodeName = (text: string) => {
-    return decodeURI(text).replaceAll('||', '/')
+    return decodeURI(text).replaceAll(' ', '_').replaceAll('||', '/')
 }
