e14a118e7439fcb9f2ecf889d060cf3c6691539e
kemelinpina <kemelinnpinadesigner@gmail.com>
2023-08-04 17:35:18 -0300

    kemi-retiradadoblog-home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -473,6 +473,7 @@ const Home = () => {
           },
         ]}
       />
+      {/* BLOG AREA
       <Flex w='100%' mt='-100px' bg='white' alignItems='center' justifyContent='center'>
         <Container maxW={['100%', '100%', '8xl', '8xl', '8xl']} mb='50px'>
         <Text color='black' fontSize='40px' fontWeight='bold' textAlign='center' mt={"5%"} mb={"5%"}>
@@ -531,7 +532,7 @@ const Home = () => {
             )}
           </Flex>
         </Container>
-      </Flex>
+      </Flex>*/}
 
       <Footer />
     </SmoothScroll>
