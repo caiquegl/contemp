84607402ef37f53edb5119ee268473e4ce13787d
kemelinpina <kemelinnpinadesigner@gmail.com>
2023-11-22 15:10:39 -0300

    kemi-variaçõesadminproduto

--- src/components/ContainerAddProduct/Variantion.tsx
+++ src/components/ContainerAddProduct/Variantion.tsx
@@ -1,4 +1,4 @@
-import { Box, HStack, Flex, Icon, UnorderedList, ListItem, Heading } from '@chakra-ui/react'
+import { Box, HStack, Flex, Icon, UnorderedList, ListItem, Heading, Badge, Wrap, Stack, Tooltip, ChakraProvider, Button, IconButton } from '@chakra-ui/react'
 import { useEffect, useRef, useState } from 'react'
 import { GrAddCircle, GrSubtractCircle } from 'react-icons/gr'
 import { useForm } from 'react-hook-form'
@@ -6,6 +6,9 @@ import { InputDefault } from '../Form/Input'
 import { v4 as uuidv4 } from 'uuid'
 import { SelectDefault } from '../Form/Select'
 import { AiOutlineArrowDown, AiOutlineArrowUp } from 'react-icons/ai'
+import { IoClose } from "react-icons/io5";
+import { FaCheck } from "react-icons/fa6";
+
 
 const Variation = ({ index, addVariation, changeOrderOpt, defaultValues, newVariation, removeVariation, removeOptVariation, addRange, saveName, setType, total, upVariation, downVariation, addPlaceholder }: any) => {
   const formRefOpt = useRef<any>()
@@ -70,7 +73,7 @@ const Variation = ({ index, addVariation, changeOrderOpt, defaultValues, newVari
             />
           </Box>
 
-         
+
           <Box w='400px' ml='20px'>
             <SelectDefault
               control={control}
@@ -103,55 +106,63 @@ const Variation = ({ index, addVariation, changeOrderOpt, defaultValues, newVari
           </Box>
         </Flex>
         <HStack spacing='20px'>
-          {index !== total && <Icon as={AiOutlineArrowDown} color="#cc0b0b" fontSize='30px' cursor='pointer' onClick={() => downVariation(index)} />}
-          {index !== 0 && <Icon as={AiOutlineArrowUp} color="#0e9721" fontSize='30px' cursor='pointer' onClick={() => upVariation(index)} />}
-          <Icon as={GrAddCircle} fontSize='30px' cursor='pointer' onClick={() => newVariation()} />
-          <Icon as={GrSubtractCircle} fontSize='30px' cursor='pointer' onClick={() => removeVariation()} />
+          <Button className='botao-preto-outline' ml={'auto'} mt={'0%!important'} rightIcon={<GrAddCircle />} fontSize={'1rem'} cursor='pointer' onClick={() => newVariation()}>
+            Adicionar Variação
+          </Button>
+          {index !== total && (
+            <IconButton aria-label='' className='botao-preto-outline' mt={'0!important'} ml={'5%!important'} mr={'2%!important'} icon={<AiOutlineArrowDown />} onClick={() => downVariation(index)}>
+            </IconButton>
+          )}
+          {index !== 0 && (
+            <IconButton aria-label='' className='botao-preto-outline' mt={'0!important'} ml={'0!important'} icon={<AiOutlineArrowUp />} onClick={() => upVariation(index)}>
+            </IconButton>
+          )}
+          <IconButton aria-label='' className='botao-vermelho' mt={'0!important'} icon={<IoClose />} onClick={() => removeVariation(index)}>
+          </IconButton>
         </HStack>
+
       </Flex>
-      {watch().type_view == 'Texto_curto'  && (
-          <Box w='400px'>
-            <InputDefault
-              label={`Nome da placeholder ${index + 1}`}
-              type='text'
-              error={errors.placeholder_name}
-              onBlurCapture={(evt) => addPlaceholder(evt.target.value)}
-              {...register('placeholder_name')}
-            />
-                      </Box>
+      {watch().type_view == 'Texto_curto' && (
+        <Box w='400px'>
+          <InputDefault
+            label={`Nome da placeholder ${index + 1}`}
+            type='text'
+            error={errors.placeholder_name}
+            onBlurCapture={(evt) => addPlaceholder(evt.target.value)}
+            {...register('placeholder_name')}
+          />
+        </Box>
 
-          )}
+      )}
 
-          
-          {watch().type_view == 'numerico'  && (
-          <Box w='400px'>
-            <InputDefault
-              label={`Nome da placeholder ${index + 1}`}
-              type='text'
-              error={errors.placeholder_name}
-              onBlurCapture={(evt) => {
-                addPlaceholder(evt.target.value)
-              }}
-              {...register('placeholder_name')}
-            />
-                      </Box>
 
-          )}
-          
-           {watch().type_view == 'Texto_longo'  && (
-          <Box w='400px'>
-            <InputDefault
-              label={`Nome da placeholder ${index + 1}`}
-              type='text'
-              error={errors.placeholder_name}
-              onBlurCapture={(evt) => addPlaceholder(evt.target.value)}
-              {...register('placeholder_name')}
-            />
-                      </Box>
+      {watch().type_view == 'numerico' && (
+        <Box w='400px'>
+          <InputDefault
+            label={`Nome da placeholder ${index + 1}`}
+            type='text'
+            error={errors.placeholder_name}
+            onBlurCapture={(evt) => {
+              addPlaceholder(evt.target.value)
+            }}
+            {...register('placeholder_name')}
+          />
+        </Box>
 
-          )}
-          
+      )}
 
+      {watch().type_view == 'Texto_longo' && (
+        <Box w='400px'>
+          <InputDefault
+            label={`Nome da placeholder ${index + 1}`}
+            type='text'
+            error={errors.placeholder_name}
+            onBlurCapture={(evt) => addPlaceholder(evt.target.value)}
+            {...register('placeholder_name')}
+          />
+        </Box>
+
+      )}
       {watch().type_view == 'Range' && (
         <Flex alignItems='center' justifyContent='space-between' w='100%'>
           <Box w='35%'>
@@ -170,45 +181,60 @@ const Variation = ({ index, addVariation, changeOrderOpt, defaultValues, newVari
       )}
       {watch().type_view == 'Number' && (
         <Flex alignItems='center' justifyContent='space-between' w='100%'>
-          <Flex alignItems='center'>
+          <Flex alignItems='center' verticalAlign={'middle'}>
             <Box w='400px'>
-              <InputDefault label='Adicionar opção' type='text' error={errors.addOpt} {...register('addOpt')} />
+              <InputDefault label='Adicionar opção' placeholder='Adicionar opção' question='Adicione a opção e  clique no botão ao lado.' type='text' error={errors.addOpt} {...register('addOpt')} />
             </Box>
-            <Icon
-              as={GrAddCircle}
-              fontSize='30px'
-              cursor='pointer'
-              ml='20px'
-              onClick={() => formRefOpt.current?.requestSubmit()}
-            />
+            <IconButton className='botao-preto' aria-label='' icon={<FaCheck />} mt={'6%'} ml={'2%'} onClick={() => formRefOpt.current?.requestSubmit()}>
+              Confirmar e Adicionar
+            </IconButton>
           </Flex>
           <Box w='100%' ml='50px'>
-            <UnorderedList>
-              {opt.map((value: any, indexOpt: number) => (
-                <ListItem key={uuidv4()} color='black.800' fontSize='18px' mb='6px'>
-                  <Flex alignItems='center'>
+            <Flex alignItems='center' justifyContent='space-between' w='100%' ml='50px'>
+              <Wrap spacing={2} align='center'>
+                {opt.map((value: any, indexOpt: number) => (
+                  <Badge
+                    key={uuidv4()}
+                    colorScheme='teal'
+                    fontSize='1rem'
+                    pt={1}
+                    pb={1}
+                    pl={6}
+                    pr={6}
+                    borderRadius='8px'
+                    color={'var(--gray-text)'}
+                    display='flex'
+                    justifyContent='space-between'
+                    alignItems='center'
+                    bgColor='var(--graylight-primary)'
+                    mb={2}
+                  >
                     {value}
-                    <Icon
-                      as={GrSubtractCircle}
-                      fontSize='20px'
-                      ml='10px'
-                      cursor='pointer'
-                      onClick={() => {
-                        removeOptVariation(indexOpt)
-                        let newOpt: any = []
-                        opt.forEach((el: any) => {
-                          if(el != value) newOpt.push(el)
-                        })
+                    <Flex alignItems='center'>
+                      <Icon as={IoClose} p={0} color={'var(--red-primary)'} fontSize='1rem' m={'0 15%'} cursor='pointer'
+                        onClick={() => {
+                          removeOptVariation(indexOpt);
+                          const newOpt = opt.filter((el: any) => el !== value);
+                          setOpt(newOpt);
+                        }} />
+                      {indexOpt !== opt.length - 1 && (
+                        <Icon as={AiOutlineArrowDown} fontWeight={'bold'} fontSize='1rem' cursor='pointer'
+                          onClick={() => downOpt(indexOpt)}
+                          color={'var(--gray-text)'}
+                          _hover={{ bg: 'red.500' }} />
+                      )}
+                      {indexOpt !== 0 && (
+                        <Icon as={AiOutlineArrowUp} fontWeight={'bold'} fontSize='1rem' cursor='pointer'
+                          onClick={() => upOpt(indexOpt)}
+                          color={'var(--gray-text)'}
+                          _hover={{ bg: 'green.500' }} />
+                      )}
+                    </Flex>
+                  </Badge>
+                ))}
+              </Wrap>
+            </Flex>
 
-                        setOpt(newOpt)
-                      }}
-                    />
-                    {indexOpt !== opt.length - 1 && <Icon as={AiOutlineArrowDown} color="#cc0b0b" fontSize='17px' cursor='pointer' onClick={() => downOpt(indexOpt)} />}
-                    {indexOpt !== 0 && <Icon as={AiOutlineArrowUp} color="#0e9721" fontSize='17px' cursor='pointer' onClick={() => upOpt(indexOpt)} />}
-                  </Flex>
-                </ListItem>
-              ))}
-            </UnorderedList>
           </Box>
         </Flex>
       )}
