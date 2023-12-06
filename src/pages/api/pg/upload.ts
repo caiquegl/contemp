bc8979ffa66c3d69c922e27fac3b38c14c347b84
kemelinpina <kemelinnpinadesigner@gmail.com>
2023-12-06 13:10:56 -0300

    kemi-uploadarquivos-exe

--- src/pages/api/pg/upload.ts
+++ src/pages/api/pg/upload.ts
@@ -1,75 +1,82 @@
-import multer from 'multer'
-import { NextApiResponse } from 'next'
-import nextConnect from 'next-connect'
-import { prisma } from '../../../lib/prisma'
+import multer from 'multer';
+import { NextApiResponse } from 'next';
+import nextConnect from 'next-connect';
+import { prisma } from '../../../lib/prisma';
+import slugify from 'slugify';
 
 const upload = multer({
   storage: multer.diskStorage({
     destination: process.env.STATUS === 'HMG' ? '/var/www/arquivos_hmg' : '/var/www/arquivos',
-    filename: (req, file, cb) => cb(null, file.originalname.toLocaleLowerCase()),
+    filename: (req, file, cb) => cb(null, file.originalname.toLowerCase()),
   }),
-})
+});
 
 const apiRoute = nextConnect({
   onError(error, req: any, res: NextApiResponse<any>) {
-    res.status(501).json({ error: `Sorry something Happened! ${error.message}` })
+    res.status(501).json({ error: `Sorry something happened! ${error.message}` });
   },
   onNoMatch(req: any, res: NextApiResponse<any>) {
-    res.status(405).json({ error: `Method '${req.method}' Not Allowed` })
+    res.status(405).json({ error: `Method '${req.method}' not allowed` });
   },
-})
+});
 
-apiRoute.use(upload.array('files'))
+apiRoute.use(upload.array('files'));
 
 apiRoute.post(async (req: any, res: NextApiResponse<any>) => {
-  const files = req.files
+  const files = req.files;
+
   try {
     for await (let file of files) {
-      const nameFile = file.originalname.toLowerCase()
-      let url = `contemp.com.br/api/arquivos/${nameFile}`.replaceAll(' ', '_')
+      const nameFile = file.originalname.toLowerCase();
+      const slugifiedName = slugify(nameFile, { replacement: '_', lower: true });
+
+      const url = `contemp.com.br/api/arquivos/${slugifiedName}`;
 
       const exist = await prisma.files.findMany({
         where: {
           name: {
-            in: nameFile
-          }
-        }
-      })
-      if (exist.length > 0) continue
+            in: nameFile,
+          },
+        },
+      });
+
+      if (exist.length > 0) continue;
+
       await prisma.files.create({
         data: {
           url: url,
           name: nameFile,
-          created_at: new Date()
-        }
-      })
+          created_at: new Date(),
+        },
+      });
     }
 
-    let user: any = JSON.parse(req.cookies['nextAuth.contemp'] as string)
-    user = user?.body?.email || ''
+    let user: any = JSON.parse(req.cookies['nextAuth.contemp'] as string);
+    user = user?.body?.email || '';
 
     await prisma.logs.create({
       data: {
         user: user,
-        description: `Subiu arquivos`
-      }
-    })
+        description: `Uploaded files`,
+      },
+    });
 
     return res.status(201).json({
-      status: true
-    })
-
+      status: true,
+    });
   } catch (error) {
-    console.log(error)
-    return res.status(201).json({
-      status: false
-    })
+    console.error(error);
+    return res.status(500).json({
+      status: false,
+      error: 'Internal server error',
+    });
   }
-})
+});
 
 export const config = {
   api: {
     bodyParser: false, // Disallow body parsing, consume as stream
   },
-}
-export default apiRoute
+};
+
+export default apiRoute;
