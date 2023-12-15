7767228962134f87924f7618b982998c247c1bfa
kemelinpina <kemelinnpinadesigner@gmail.com>
2023-12-15 10:49:46 -0300

    kemi-banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,162 +1,70 @@
-import {
-  Box,
-  Container,
-  Flex,
-  Text,
-  Link,
-  IconButton,
-  useBreakpointValue,
-  Stack,
-  Button,
-  Heading,
-} from '@chakra-ui/react'
-import { Image } from './Image'
-import React, { useEffect, useState } from 'react'
-import Team from '../assets/images/temnacontemp.png'
-import { Typewriter } from 'react-simple-typewriter'
-import { pxToRem } from '../utils/pxToRem'
-import { SearchBar } from './SearchBar'
-import { useWindowSize } from '../utils/useWindowSize'
-import BGFavorite from '../assets/banners/Banner2.png'
-import { BiLeftArrowAlt, BiRightArrowAlt } from 'react-icons/bi'
-// Here we have used react-icons package for the icons
-import Slider from 'react-slick'
-// And react-slick as our Carousel Lib
-// Settings for the slider
-const settings = {
-  dots: true,
-  arrows: false,
-  fade: true,
-  infinite: true,
-  autoplay: true,
-  speed: 500,
-  autoplaySpeed: 4000,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-}
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 
 export function Banner() {
-  // As we have used custom buttons, we need a reference variable to
-  // change the state
-  const [slider, setSlider] = React.useState<Slider | null>(null)
-
-  // These are the breakpoints which changes the position of the
-  // buttons as the screen size changes
-  const top = useBreakpointValue({ base: '90%', md: '50%' })
-  const side = useBreakpointValue({ base: '30%', md: '40px' })
-
-  // This list contains all the data for carousels
-  // This can be static or loaded from a server
-  const cards: any = [
+  const cards = [
     {
-      subtitle:'',
+      subtitle: '',
       title: '',
-      text: "",
-      image:
-        'https://contemp.com.br/api/arquivos/bannercontemp.webp',
+      text: '',
+      image: 'https://contemp.com.br/api/arquivos/bannercontemp.webp',
       links: '#',
     },
     {
-      subtitle:'MEDIÇÃO E CONTROLE DE TEMPERATURA EM',
+      subtitle: 'MEDIÇÃO E CONTROLE DE TEMPERATURA EM',
       title: 'PROCESSOS INDUSTRIAIS',
-      text: "Conheça nossas soluções completas para sua indústria. Fale com nossa equipe de vendas.",
-      image:
-        'https://contemp.com.br/api/arquivos/banner-2.webp',
-        links: '#',
+      text: 'Conheça nossas soluções completas para sua indústria. Fale com nossa equipe de vendas.',
+      image: 'https://contemp.com.br/api/arquivos/banner-2.webp',
+      links: '#',
     },
     {
-      subtitle:'',
+      subtitle: '',
       title: 'ATENDEMOS O BRASIL E A AMÉRICA LATINA',
-      text: "Temos uma equipe de vendedores-técnicos de prontidão para te atender.",
-      image:
-        'https://contemp.com.br/api/arquivos/banner-3.webp',
-        links: 'https://contemp.com.br/category/PIRÔMETROS_INFRAVERMELHOS_FIXOS',
-    }
-  ]
+      text: 'Temos uma equipe de vendedores-técnicos de prontidão para te atender.',
+      image: 'https://contemp.com.br/api/arquivos/banner-3.webp',
+      links: 'https://contemp.com.br/category/PIRÔMETROS_INFRAVERMELHOS_FIXOS',
+    },
+  ];
 
   return (
-    <Box
-      position={'relative'}
-      height={['450px']}
-      width={'full'}
-      overflow={'hidden'}
-      display={['none', 'block']}
-      zIndex={2}
+    <Swiper
+      spaceBetween={50}
+      slidesPerView={1}
+      navigation
+      pagination={{ clickable: true }}
     >
-      {/* CSS files for react-slick */}
-      <link
-        rel='stylesheet'
-        type='text/css'
-        href='https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css'
-      />
-      <link
-        rel='stylesheet'
-        type='text/css'
-        href='https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css'
-      />
-      {/* Left Icon */}
-      <IconButton
-        aria-label='left-arrow'
-        variant='ghost'
-        position='absolute'
-        left={side}
-        top={top}
-        transform={'translate(0%, -50%)'}
-        zIndex={2}
-        onClick={() => slider?.slickPrev()}
-      >
-        <BiLeftArrowAlt size='40px' />
-      </IconButton>
-      {/* Right Icon */}
-      <IconButton
-        aria-label='right-arrow'
-        variant='ghost'
-        position='absolute'
-        right={side}
-        top={top}
-        transform={'translate(0%, -50%)'}
-        zIndex={2}
-        onClick={() => slider?.slickNext()}
-      >
-        <BiRightArrowAlt size='40px' />
-      </IconButton>
-      {/* Slider */}
-      <Slider {...settings} ref={(slider) => setSlider(slider)}>
-        {cards.map((card: any, index: any) => (
-          <Box
-            key={index}
-            position='relative'
-            backgroundPosition='center'
-            backgroundRepeat='no-repeat'
-            backgroundSize='cover'
-            backgroundImage={`url(${card.image})`}
+      {cards.map((card, index) => (
+        <SwiperSlide key={`slide-${index + 1}`} id={`slide-${index + 1}`}>
+              <a href={card.links} target="_blank" rel="noopener noreferrer">
+          <div
+            style={{
+              position: 'relative',
+              backgroundPosition: 'center',
+              backgroundRepeat: 'no-repeat',
+              backgroundSize: 'cover',
+              backgroundImage: `url(${card.image})`,
+              height: '450px',
+            }}
           >
-            <Link href={`${card.links}`} isExternal>
-              {/* This is the block you need to change, to customize the caption */}
-              <Container height={['450px']} position='relative'>
-                <Stack
-                  spacing={5}
-                  w={'full'}
-                  maxW={'1240px'}
-                  position='absolute'
-                  top='50%'
-                  transform='translate(0, -50%)'
-                >
-                  <Heading as={'h3'} className='bannerhome-subtitulo negrito text-white centro'>
-                    {card.subtitle}
-                  </Heading>
-                  <Heading as={'h2'} className='bannerhome-titulo negrito text-white centro'>
-                    {card.title}
-                  </Heading>
-                  <Text fontSize={{ base: 'md', lg: 'lg' }} className='bannerhome-descricao text-white centro'>
-                    {card.text}
-                  </Text>
-                </Stack>
-              </Container>
-            </Link>
-          </Box>
-        ))}
-      </Slider>
-    </Box>
-  )
+              <div
+                style={{
+                  position: 'absolute',
+                  top: '50%',
+                  left: '50%',
+                  transform: 'translate(-50%, -50%)',
+                  textAlign: 'center',
+                  color: '#fff',
+                }}
+              >
+                <h3 style={{ fontSize: '1.5rem', fontWeight: 'bold', marginBottom: '10px' }}>{card.subtitle}</h3>
+                <h2 style={{ fontSize: '2rem', fontWeight: 'bold', marginBottom: '10px' }}>{card.title}</h2>
+                <p style={{ fontSize: '1rem' }}>{card.text}</p>
+              </div>
+          </div>
+            </a>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
 }
