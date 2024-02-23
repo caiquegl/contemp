3da3f3c8d7d61370fe5f979d153b4b768ed77263
caiquegl <arq.caique@hotmail.com>
2024-02-23 10:21:29 -0300

    deploy

--- src/pages/api/get-files-backup-2.ts
+++ src/pages/api/get-files-backup-2.ts
@@ -2,36 +2,9 @@ import axios from 'axios';
 import fs from 'fs';
 import path from 'path';
 import { NextApiRequest, NextApiResponse } from 'next';
-import { prisma } from '../../lib/prisma';
-import multer from 'multer';
-import nextConnect from 'next-connect';
-import { createSlug, SlugifyOptions } from './pg/slugfyfile';
-import { dbContemp } from './database_contemp' // Importe a função de slugify
+import { dbContemp } from './database_contemp';
 
-// Configuração do multer para o armazenamento de arquivos
-const upload = multer({
-  storage: multer.diskStorage({
-    destination: process.env.STATUS === 'HMG' ? '/var/www/html/arquivos_hmg' : '/var/www/html/arquivos',
-    filename: (req, file, cb) => {
-      // Opções para a função de criação de slug
-      const slugOptions: SlugifyOptions = {
-        replacementChar: '_',
-        removePunctuation: true,
-        maxLength: 1500, // Ajuste conforme necessário
-      };
-
-      // Criar um nome de arquivo slugificado usando a função
-      const slugifiedName = createSlug(file.originalname, slugOptions);
-      cb(null, slugifiedName);
-    },
-  }),
-  limits: {
-    fileSize: 1073741824, // 10 megabytes (ajuste conforme necessário)
-  },
-});
-
-// Função para baixar um arquivo de uma URL e salvá-lo localmente
-async function downloadFile(url: any, localPath: any) {
+async function downloadFile(url: string, localPath: string) {
   const writer = fs.createWriteStream(localPath);
 
   const response = await axios({
@@ -48,48 +21,42 @@ async function downloadFile(url: any, localPath: any) {
   });
 }
 
-// Dentro do seu manipulador de rota
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+function getFileNameFromUrl(url: string) {
+  const filenameMatch = url.match(/\/([^\/?#]+)[^\/]*$/);
+  if (!filenameMatch) {
+    throw new Error('Não foi possível extrair o nome do arquivo da URL.');
+  }
+  const fileNameEncoded = filenameMatch[1].split('?')[0];
+  const fileNameDecoded = decodeURIComponent(fileNameEncoded);
+  const extension = fileNameDecoded.split('.')[1].split('-')[0];
+  return `${fileNameDecoded}.${extension}`;
+}
+
+export default async function handler(req: any, res: any) {
   try {
-    const all = await dbContemp('home')
+    const all = await dbContemp('categories');
 
     for await (let exist of all) {
       if (!exist) {
         throw new Error('Nenhum registro encontrado');
       }
 
-      await dbContemp('home')
-        .update('backup_icon', exist.icon)
-        .where('id', exist.id)
+      await dbContemp('categories')
+        .update({
+          'backup_urlPicture': exist.backup_urlPicture ? undefined :  exist.urlPicture,
+        })
+        .where('id', exist.id);
 
-      const iconUrl = exist.icon;
-
-      // Extrair o nome do arquivo da URL
-      const filenameMatch = iconUrl.match(/\/([^\/?#]+)[^\/]*$/);
-      if (!filenameMatch) {
-        throw new Error('Não foi possível extrair o nome do arquivo da URL.');
-      }
-// Extrair o nome do arquivo da URL
-      const urlParts = iconUrl.split('/');
-      const fileNameEncoded = urlParts[urlParts.length - 1].split('?')[0]; // Remove os parâmetros de consulta da URL
-      let fileName = decodeURIComponent(fileNameEncoded);
-
-      const extension = fileNameEncoded.split('.')[1].split('-')[0]
-      fileName = `${fileName}.${extension}`
-      console.log(fileName, fileNameEncoded)
-// Verificar se o nome do arquivo foi obtido com sucesso
-      if (!fileName) {
-        throw new Error('Não foi possível obter o nome do arquivo da URL.');
-      }
-
-      // Download do arquivo de imagem
+      const url = exist.urlPicture;
+      const fileName = getFileNameFromUrl(url);
       const localFilePath = path.join(process.env.STATUS === 'HMG' ? '/var/www/html/arquivos_hmg' : '/var/www/html/arquivos', fileName);
-      await downloadFile(iconUrl, localFilePath);
+      await downloadFile(url, localFilePath);
 
-      const baseUrl = process.env.STATUS === 'HMG' ? 'https://hmg.contemp.com.br/' : 'https://contemp.com.br/'
-      await dbContemp('home')
-        .update('icon', `${baseUrl}api/pictures/${fileName}`)
-        .where('id', exist.id)
+      const baseUrl = process.env.STATUS === 'HMG' ? 'https://hmg.contemp.com.br/' : 'https://contemp.com.br/';
+      await dbContemp('categories')
+        .update('urlPicture', `${baseUrl}api/pictures/${fileName}`)
+        .where('id', exist.id);
+
     }
     return res.json({ success: true });
   } catch (err) {
@@ -98,11 +65,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   }
 }
 
-// Configuração da rota API para fazer o upload de arquivos
 export const config = {
   api: {
     bodyParser: false,
   },
 };
-
-// Utilizar o middleware de upload para manipular arquivos
